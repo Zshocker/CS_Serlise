007b33600f5078aa419d1a7a0e9b852fda8a60b1
Zshocker <hicham.sentel-etu@etu.univh2c.ma>
2022-02-09 11:18:57 +0100

    Add project files.

---
+++ TestSeri/Etudiant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSeri
+{
+    class Etudiant
+    {
+        public string name { get; set; }
+        public List<Etudiant> ListAmis { get; set; } 
+        public Etudiant(string nam)
+        {
+            name = nam;
+            ListAmis = new List<Etudiant>();
+        }
+        public Etudiant()
+        {
+        }
+        public void add_amie(Etudiant etudiant)
+        {
+            ListAmis.Add(etudiant);
+        }
+    }
+}
---
+++ TestSeri/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json.Net;
+using System.IO;
+
+
+namespace TestSeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Etudiant Hicham= new Etudiant("Hicham");
+            Etudiant Khad= new Etudiant("Khadija");
+            Etudiant moh = new Etudiant("Mohamed");
+            Etudiant Hiba = new Etudiant("Hiba");
+            Etudiant Imane= new Etudiant("Imane");
+            Etudiant Hamid= new Etudiant("Amine");
+            Etudiant Mehd= new Etudiant("Mehdi");
+            Hicham.add_amie(Mehd);
+            Hicham.add_amie(moh);
+            Hicham.add_amie(Hiba);
+            Hicham.add_amie(Khad);
+            Hicham.add_amie(Imane);
+            Hicham.add_amie(Hamid); 
+            File.WriteAllText("Hicham.json", JsonNet.Serialize(Hicham));
+            Etudiant copie = JsonNet.Deserialize<Etudiant>(File.ReadAllText("Hicham.json"));
+            Console.ReadLine();
+        }
+    }
+}
